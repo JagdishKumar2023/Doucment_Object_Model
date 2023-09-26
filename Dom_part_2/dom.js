496ef3c4434c7b5d89ec91ad769a4489d6ca352e
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-26 20:46:38 +0530

    New Changes

---
+++ Dom_part_2/dom.js
@@ -0,0 +1,19 @@
+const myBody = document.body;
+
+console.log(myBody);
+
+const box2 = document.getElementById("box-2");
+
+console.log(box2);
+
+const divs = document.getElementsByTagName("p");
+
+console.log(divs);
+
+const boxes = document.getElementsByClassName("Random");
+
+console.log(boxes);
+
+const Random = document.querySelectorAll(".container.random");
+
+console.log(boxes);
