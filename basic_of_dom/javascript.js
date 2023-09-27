2803ca0eb5a76cc3ea9fdd06e847c228a6275c16
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-09-27 21:12:08 +0530

    New Changes

---
+++ basic_of_dom/javascript.js
@@ -0,0 +1,10 @@
+// var a = document.querySelector("h1");
+// a.innerHTML = "Kaie ho ap log";
+// a.style.color = "blue";
+// a.style.backgroundColor = "yellow";
+
+var h = document.querySelectorAll("h1");
+
+h.forEach(function (e) {
+  console.log(e);
+});
